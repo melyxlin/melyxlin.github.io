b1e2e10b597dc8569757fb5cbfb7936a803dfe75
momolin78 <momolin78@gmail.com>
2020-05-28 22:39:15 -0400

    reformat

--- js/main.js
+++ js/main.js
@@ -8,23 +8,12 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("header").style.display = "block"
     document.getElementById("particles-js").style.display = "block"
-    if (document.body.classList.contains('aboutpage')) {
-        document.getElementById("about").style.display = "block"
-
-    }
-    else if (document.body.classList.contains('projectspage')) {
         document.getElementById("projects").style.display = "block"
-
-    }
-    else if (document.body.classList.contains('indexpage')) {
         document.getElementById("landing").style.display = "block"
-
-    }
-    else if (document.body.classList.contains('workpage')) {
-        document.getElementById("work").style.display = "block"
-
-    }
+        document.getElementById("about").style.display = "block"
+        document.getElementById("contact").style.display = "block";
 
     document.getElementById("footer").style.display = "block";
-    document.getElementById("icon-bar").style.display = "block";
+    document.getElementById("scrollup").style.display = "block";
+    document.getElementById("scrolldown").style.display = "block";
 }
